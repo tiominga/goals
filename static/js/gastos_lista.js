67c8d87522b87502b9a49348599219e40429067f
DESKTOP-E4A5MOE\Luis Miguel <tiominga@gmail.com>
2024-01-10 17:59:55 -0300

    com datatable na lista de gastos V1.2

--- static/js/gastos_lista.js
+++ static/js/gastos_lista.js
@@ -1,3 +1,38 @@
+function loaded(){
+
+  $('#tb_gastos_lista').DataTable({
+    responsive: true,
+    // "bSort": false,
+    "aaSorting": [],
+    "pageLength": 10,
+    "language": {
+        "decimal": "",
+        "emptyTable": "Sem dados disponíveis",
+        "info": "Mostrando de _START_ até _END_ de _TOTAL_ registos",
+        "infoEmpty": "Mostrando de 0 até 0 de 0 registos",
+        "infoFiltered": "(filtrado de MAX registos no total)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ registos",
+        "loadingRecords": "A carregar dados...",
+        "processing": "A processar...",
+        "search": "Procurar:",
+        "zeroRecords": "Não foram encontrados resultados",
+        "paginate": {
+            "first": "Primeiro",
+            "last": "Último",
+            "next": "Seguinte",
+            "previous": "Anterior"
+        },
+        "aria": {
+            "sortAscending": ": ordem crescente",
+            "sortDescending": ": ordem decrescente"
+        }
+    }
+});
+
+}
+
 function  liveToast(bg,message){
 
     $('#liveToast').removeClass('bg-primary bg-secondary bg-success bg-danger bg-warning bg-info bg-light bg-dark');
