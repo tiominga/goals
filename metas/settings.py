31097a834a58dba4553f060f898dab214f0030ef
DESKTOP-E4A5MOE\Luis Miguel <tiominga@gmail.com>
2024-01-11 09:12:37 -0300

    sem zero ao adicionar gasto

--- metas/settings.py
+++ metas/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1%gk%b8e6ugzb@*4&+6bfjvqviot3hwmh_hxnht3@678wge9nv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
