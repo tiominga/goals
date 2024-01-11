e9c8a679cfe878329ae5d3e6216e2867740125f0
DESKTOP-E4A5MOE\Luis Miguel <tiominga@gmail.com>
2024-01-11 09:15:18 -0300

    com debug

--- metas/settings.py
+++ metas/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1%gk%b8e6ugzb@*4&+6bfjvqviot3hwmh_hxnht3@678wge9nv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
